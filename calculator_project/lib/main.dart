a564b3c5030e6b2383d22f590be135b8de333327
Felipe <felipesakaiipolito@gmail.com>
2024-09-13 16:08:46 -0400

    a

---
+++ calculator_project/lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculadoraApp());
+}
+
+class CalculadoraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora Simples',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CalculadoraHomePage(),
+    );
+  }
+}
+
+class CalculadoraHomePage extends StatefulWidget {
+  @override
+  _CalculadoraHomePageState createState() => _CalculadoraHomePageState();
+}
+
+class _CalculadoraHomePageState extends State<CalculadoraHomePage> {
+  String _output = "0";
+  String _expression = "";
+  double num1 = 0;
+  double num2 = 0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+      _expression = "";
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "/" || buttonText == "x") {
+      num1 = double.parse(_output);
+      operand = buttonText;
+      _output = "0";
+      _expression += buttonText;
+    } else if (buttonText == ".") {
+      if (!_output.contains(".")) {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(_output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      } else if (operand == "-") {
+        _output = (num1 - num2).toString();
+      } else if (operand == "x") {
+        _output = (num1 * num2).toString();
+      } else if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      _output = double.parse(_output).toString();
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        style: OutlinedButton.styleFrom(
+          padding: EdgeInsets.all(24.0),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+            child: Text(
+              _output,
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  buildButton("7"),
+                  buildButton("8"),
+                  buildButton("9"),
+                  buildButton("/"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("4"),
+                  buildButton("5"),
+                  buildButton("6"),
+                  buildButton("x"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("1"),
+                  buildButton("2"),
+                  buildButton("3"),
+                  buildButton("-"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("."),
+                  buildButton("0"),
+                  buildButton("00"),
+                  buildButton("+"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  buildButton("C"),
+                  buildButton("="),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
